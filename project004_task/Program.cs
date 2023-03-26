47d7db623ead47b92e0ff9e31917437bc364ab51
ReZero <ghostofpast@icloud.com>
2023-03-26 20:38:53 +0300

    end task pj

---
+++ project004_task/Program.cs
@@ -0,0 +1,34 @@
+﻿//На ввод даётся два числа, ищем меньшее, к нему прибавляем разницу между большим и меньшим столко раз, какова величина меньшего числа
+
+int first = new Random().Next(1,1000);
+Console.WriteLine("Первое число: " + first);
+int second = new Random().Next(1,1000);
+Console.WriteLine("Второе число: " + second);
+
+int max = 0;
+int min = 0;
+
+if (first > second)
+{
+    max = first;
+    min = second;
+}
+else
+{
+    max = second;
+    min = first;
+}
+
+Console.WriteLine("Max: " + max);
+Console.WriteLine("Min: " + min);
+
+int count = 0;
+int result = min;
+
+while(count < min)
+{
+    result = result + (max - min);
+    count++;
+}
+
+Console.WriteLine("Результат: " + result);
