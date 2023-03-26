1281ca374f6b4c5a958ef4f7e80481d46b073363
ReZero <ghostofpast@icloud.com>
2023-03-26 17:14:31 +0300

    end expon pj

--- project001_expon/Program.cs
+++ project001_expon/Program.cs
@@ -1,1 +1,12 @@
-﻿int i = int.Parse("1");
+﻿Console.Write("Введите число для возведения в квадрат: ");
+
+int n = 0;
+
+if (int.TryParse(Console.ReadLine(), out n))
+{
+    Console.WriteLine(n * n);
+}
+else
+{
+    Console.WriteLine("Неверный ввод");
+}
