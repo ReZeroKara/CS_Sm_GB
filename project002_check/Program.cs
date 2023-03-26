44955a9526602f85bc4d4f1f7437fef042317fa1
ReZero <ghostofpast@icloud.com>
2023-03-26 18:38:20 +0300

    add check pj

---
+++ project002_check/Program.cs
@@ -0,0 +1,53 @@
+﻿//Программа для проверки: является ли одно число квадратом второго
+
+int a = 25;
+int b = 5;
+int c = b * b;
+
+if(a == c)
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
+
+int aa = 2;
+int bb = 10;
+int cc = bb * bb;
+
+if(aa == cc)
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
+
+int aaa = 9;
+int bbb = (-3);
+int ccc = bbb * bbb;
+
+if(aaa == ccc)
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
+
+int aaaa = (-3);
+int bbbb = 9;
+int cccc = bbbb * bbbb;
+
+if(aaaa == cccc)
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
