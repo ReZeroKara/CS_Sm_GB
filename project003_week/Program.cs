3cc1d7d4802d6be8605e7f18596682da5b89af4c
ReZero <ghostofpast@icloud.com>
2023-03-26 20:59:34 +0300

    upd week pj

--- project003_week/Program.cs
+++ project003_week/Program.cs
@@ -8,38 +8,35 @@ if (day == 1)
 {
     Console.WriteLine("Понедельник");
 }
-
-if (day == 2)
+else if (day == 2)
 {
     Console.WriteLine("Вторник");
 }
-
-if (day == 3)
+else if (day == 3)
 {
     Console.WriteLine("Среда");
 }
-
-if (day == 4)
+else if (day == 4)
 {
     Console.WriteLine("Четверг");
 }
-
-if (day == 5)
+else if (day == 5)
 {
     Console.WriteLine("Пятница");
 }
-
-if (day == 6)
+else if (day == 6)
 {
     Console.WriteLine("Суббота");
 }
-
-if (day == 7)
+else if (day == 7)
 {
     Console.WriteLine("Воскресенье");
 }
-
-if (day > 7)
+else if (day > 7)
 {
     Console.WriteLine("Столько дней в неделе не бывает;)");
+}
+else
+{
+    Console.WriteLine("Не правильный ввод!");
 }
