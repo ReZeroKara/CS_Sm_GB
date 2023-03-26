7d6713cdda800518a607b3d355791b7f498f82c2
ReZero <ghostofpast@icloud.com>
2023-03-26 19:12:48 +0300

    add enter numbers

--- project002_check/Program.cs
+++ project002_check/Program.cs
@@ -1,49 +1,14 @@
 ﻿//Программа для проверки: является ли одно число квадратом второго
 
-int a = 25;
-int b = 5;
+Console.Write("Введите первое число: ");
+int a = 0;
+int.TryParse(Console.ReadLine(), out a);
+Console.Write("Введите второе число: ");
+int b = 0;
+int.TryParse(Console.ReadLine(), out b);
 int c = b * b;
 
 if(a == c)
-{
-    Console.WriteLine("Первое число является квадратом второго.");
-}
-else
-{
-    Console.WriteLine("Первое число не является квадратом второго.");
-}
-
-int aa = 2;
-int bb = 10;
-int cc = bb * bb;
-
-if(aa == cc)
-{
-    Console.WriteLine("Первое число является квадратом второго.");
-}
-else
-{
-    Console.WriteLine("Первое число не является квадратом второго.");
-}
-
-int aaa = 9;
-int bbb = (-3);
-int ccc = bbb * bbb;
-
-if(aaa == ccc)
-{
-    Console.WriteLine("Первое число является квадратом второго.");
-}
-else
-{
-    Console.WriteLine("Первое число не является квадратом второго.");
-}
-
-int aaaa = (-3);
-int bbbb = 9;
-int cccc = bbbb * bbbb;
-
-if(aaaa == cccc)
 {
     Console.WriteLine("Первое число является квадратом второго.");
 }
