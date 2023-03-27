3d5719c7befa290d46822171ebe414ba32271bad
ReZero <ghostofpast@icloud.com>
2023-03-27 14:39:08 +0300

    add lastnum pj

---
+++ project006_lastnum/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа, которая принимает на вход трёхзначиное число и на выходе показывает последнюю цифру
+
+Console.Write("Введите трёхзначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+if(n < 100)
+{
+    Console.WriteLine("Неверный ввод!");
+}
+else if(n > 999)
+{
+    Console.WriteLine("Неверный ввод!");
+}
+else
+{
+    int result = n % 10;
+    Console.WriteLine(result);
+}
