c17d19a4d0bc0f127da07f6ab243d3a0c400c249
ReZero <ghostofpast@icloud.com>
2023-03-27 14:24:16 +0300

    add numbers pj

---
+++ project005_numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа принимает на вход число n и показывает все числа в диапазоне -n до n
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int count1 = 0;
+int count2 = (n * 2) + 1;
+int a = 0;
+
+while(count1 < count2)
+{
+    int result = a - n;
+    Console.Write(result + ", ");
+    a++;
+    count1++;
+}
